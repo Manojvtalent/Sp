18b098b8a64094c464b017dc1d7adf2cb2f95fb0
manoj goud kurmindla <manojgoud.vtalent@gmail.com>
2024-07-17 10:40:32 +0530

    Add project files.

---
+++ Sp/Controllers/EmpController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Protocols;
+using Sp.Model;
+using System.Data.SqlClient;
+using System.Configuration;
+using Microsoft.Extensions.Configuration;
+using System.Threading.Tasks;
+using System.Data;
+using System;
+
+namespace Sp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmpController : Controller
+    {
+        private IConfiguration config;
+        public EmpController(IConfiguration config)
+        {
+            this.config = config;
+        }
+       
+
+        
+        [HttpPost]
+        public async Task<IActionResult> post(employee emp)
+        {
+            //var conntionString = config.GetConnectionString("Con");
+            //var conn = new SqlConnection(conntionString);
+            var conn =new SqlConnection(config.GetConnectionString("Con"));
+            try
+            {
+
+            
+            if (emp != null)
+            {
+                await conn.OpenAsync(); 
+                SqlCommand cmd = new SqlCommand("AddNewEmp", conn);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cmd.Parameters.AddWithValue("@name", emp.name);
+                cmd.Parameters.AddWithValue("@Email", emp.Email);
+
+                await cmd.ExecuteNonQueryAsync();
+                   
+
+            }
+                return Ok("record created");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+
+
+        }
+    }
+}
---
+++ Sp/Model/employee.cs
@@ -0,0 +1,9 @@
+﻿namespace Sp.Model
+{
+    public class employee
+    {
+        public int spId {  get; set; }
+        public string name { get; set; }
+        public string Email { get; set; }
+    }
+}
